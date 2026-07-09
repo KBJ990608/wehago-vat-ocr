76d4fa8e893fdb84b19750db015ed54497ef4520
KBJ990608 <266233858+KBJ990608@users.noreply.github.com>
2026-07-09 12:57:19 +0900

    Reduce Electron package size

--- forge.config.cjs
+++ forge.config.cjs
@@ -1,3 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const keepLocaleFiles = new Set(['ko.pak', 'en-US.pak', 'en-GB.pak']);
+
+function pruneUnusedLocales(buildPath, electronVersion, platform, arch, callback) {
+  if (platform !== 'win32') {
+    callback();
+    return;
+  }
+
+  const localesDir = path.join(buildPath, 'locales');
+
+  try {
+    if (fs.existsSync(localesDir)) {
+      for (const fileName of fs.readdirSync(localesDir)) {
+        if (fileName.endsWith('.pak') && !keepLocaleFiles.has(fileName)) {
+          fs.rmSync(path.join(localesDir, fileName), { force: true });
+        }
+      }
+    }
+
+    callback();
+  } catch (error) {
+    callback(error);
+  }
+}
+
 module.exports = {
   packagerConfig: {
     name: 'WEHAGO VAT OCR',
@@ -5,6 +33,7 @@ module.exports = {
     icon: 'assets/icon',
     asar: true,
     overwrite: true,
+    afterComplete: [pruneUnusedLocales],
     ignore: [
       /^\/\.env(?:$|\.)/,
       /^\/\.git(?:$|\/)/,
